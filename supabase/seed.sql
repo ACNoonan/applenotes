c05b536b2243515d91e4e149de7eaeaeb270100b
Adam Noonan <adamcnoonan@gmail.com>
2025-04-03 18:46:55 +0200

    Changing SQL file

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,15 +1,15 @@
-INSERT INTO public.notes (
-    title,
-    content,
-    public,
-    slug,
-    emoji,
-    created_at
-) VALUES (
-    'Thank You Alana!',
-    'Thank you for taking the time to meet with me today. Your insights and guidance were incredibly valuable.',
-    true, -- making it public
-    'thank-you-alana',
-    '🙏',
-    CURRENT_TIMESTAMP
-);
+   INSERT INTO public.notes (
+       title,
+       content,
+       public,
+       slug,
+       emoji,
+       created_at
+   ) VALUES (
+       'thank you alana!',
+       'thanks so much for your time and advice today - it means the world to me!',
+       true,
+       'thank-you-alana',
+       '🙏',
+       CURRENT_TIMESTAMP
+   );
